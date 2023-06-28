4e3275d09e318b6a0f6b78b462e0900b25e38faf
Urvishvaghasiya777 <urvishpatel204@gmail.com>
2023-06-28 11:25:22 +0530

    filess

--- src/admin/SignUpShow.js
+++ src/admin/SignUpShow.js
@@ -1,39 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
+import axios from 'axios';
 
 const columns = [
   {
-      name: 'Title',
-      selector: row => row.title,
+      name: 'name',
+      selector: row => row.name,
       sortable:true
       
   },
   {
-      name: 'Year',
-      selector: row => row.year,
+    name: 'email',
+    selector: row => row.email,
+    sortable:true
+    
+},
+{
+  name: 'contact',
+  selector: row => row.contact,
+  sortable:true
+  
+},
+  {
+      name: 'city',
+      selector: row => row.city,
       sortable:true
   },
 ];
 
-const data = [
-  {
-      id: 1,
-      title: 'Beetlejuice',
-      year: '1988',
-  },
-  {
-      id: 2,
-      title: 'Ghostbusters',
-      year: '1984',
-  },
-]
+// const data = [
+//   {
+//       id: 1,
+//       title: 'Beetlejuice',
+//       year: '1988',
+//   },
+//   {
+//       id: 2,
+//       title: 'Ghostbusters',
+//       year: '1984',
+//   },
+// ]
 const SignUpShow = () => {
+  const [table,setTable] =useState([]);
+
+  useEffect(() => {
+
+    axios.get('http://localhost:8000/signup/getregister')
+  .then(function (response) {
+    // handle success
+    console.log(response.data.data);
+    setTable(response.data.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+  },[])
 
   return (
     <>
      <DataTable
             columns={columns}
-            data={data}
+            data={table}
+            
             pagination
             selectableRows
             expandableRows
