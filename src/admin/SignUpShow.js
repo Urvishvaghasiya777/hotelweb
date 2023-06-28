58cf6b5641bd0a196aef7f3575d541285f8f6043
Urvishvaghasiya777 <urvishpatel204@gmail.com>
2023-06-28 14:08:35 +0530

    update

--- src/admin/SignUpShow.js
+++ src/admin/SignUpShow.js
@@ -1,30 +1,27 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
-import axios from 'axios';
+import axios from "axios";
 
 const columns = [
   {
-      name: 'name',
-      selector: row => row.name,
-      sortable:true
-      
+    name: "name",
+    selector: (row) => row.name,
+    sortable: true,
   },
   {
-    name: 'email',
-    selector: row => row.email,
-    sortable:true
-    
-},
-{
-  name: 'contact',
-  selector: row => row.contact,
-  sortable:true
-  
-},
+    name: "email",
+    selector: (row) => row.email,
+    sortable: true,
+  },
   {
-      name: 'city',
-      selector: row => row.city,
-      sortable:true
+    name: "contact",
+    selector: (row) => row.contact,
+    sortable: true,
+  },
+  {
+    name: "city",
+    selector: (row) => row.city,
+    sortable: true,
   },
 ];
 
@@ -41,43 +38,60 @@ const columns = [
 //   },
 // ]
 const SignUpShow = () => {
-  const [table,setTable] =useState([]);
+  const [table, setTable] = useState([]);
+
+  const [search, setSerach] = useState(); // search ============
+
+  const allData = () => {
+    axios
+      .get("http://localhost:8000/signup/getregister")
+      .then(function (response) {
+        // handle success
+        console.log(response.data.data);
+        setTable(response.data.data);
+        setSerach(response.data.data); // search=========
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
+    allData();
+  }, []);
+// search bar ====================
+  const handlerSearch = (item) => {
+    item = item.toLowerCase();
+    let dataSearch = table.filter((e) => {
+      return e.name.toLowerCase().includes(item);
+    });
+    setSerach(dataSearch);
+  };
 
-    axios.get('http://localhost:8000/signup/getregister')
-  .then(function (response) {
-    // handle success
-    console.log(response.data.data);
-    setTable(response.data.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
-  },[])
+  //===================
 
   return (
     <>
-     <DataTable
-            columns={columns}
-            data={table}
-            
-            pagination
-            selectableRows
-            expandableRows
-            fixedHeader
-            
-            fixedHeaderScrollHeight="200px"
-            subHeader
-            subHeaderComponent={
-            <input
+      <DataTable
+        columns={columns}
+        data={search} // search
+        pagination
+        selectableRows
+        expandableRows
+        fixedHeader
+        // fixedHeaderScrollHeight="200px"
+        subHeader
+        subHeaderComponent={
+          <input      // search
             type="text"
-            
-            />
-            }
-        />
+            placeholder="Search"
+            onChange={(e) => {
+              handlerSearch(e.target.value);
+            }}
+          />
+        }
+      />
     </>
   );
 };
